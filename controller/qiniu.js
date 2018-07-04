a14dfd74d633b792b0cfc37633f8695b6fdbb6c3
tujiaw <tujiaw@163.com>
2018-07-04 10:31:46 +0800

    qiniu

--- controller/qiniu.js
+++ controller/qiniu.js
@@ -4,14 +4,15 @@ var qiniu = require('qiniu');
 var config = require('../config');
 const uuidV1 = require('uuid/v1');
 
-qiniu.conf.ACCESS_KEY = config.qiniu.ak;
-qiniu.conf.SECRET_KEY = config.qiniu.sk;
+var mac = new qiniu.auth.digest.Mac(config.qiniu.ak, config.qiniu.sk);
 //构建上传策略函数
 function getUptoken(bucket, key) {
-  //  var putPolicy = new qiniu.rs.PutPolicy(bucket + ":" + key);
-  var putPolicy = new qiniu.rs.PutPolicy(bucket);
-  putPolicy.saveKey = uuidV1();
-  return putPolicy.token();
+  var options = {
+      scope: bucket
+  };
+  var putPolicy = new qiniu.rs.PutPolicy(options);
+  var uploadToken=putPolicy.uploadToken(mac);
+  return uploadToken
 }
 
 module.exports.uptoken = async function(ctx) {
