edc15e38e53fe3c4a539b6f831098de93a30d4bb
tujiaw <tujiaw@163.com>
2018-08-29 09:34:40 +0800

    add

--- controller/comments.js
+++ controller/comments.js
@@ -2,25 +2,25 @@ const xss = require("xss");
 const CommentsModel = require('../models/comments');
 
 module.exports.add = async function(ctx) {
-    let data = ctx.request.body;
-    let error = '';
+    let result = '';
     try {
+        let data = JSON.parse(ctx.request.body);
         if (data.postId.length && data.name.length && data.content.length) {
             const count = await CommentsModel.countByPostId(data.postId)
             if (count >= 1000) {
-                error = '评论过多禁止发表新的评论'
+                result = '评论过多禁止发表新的评论'
             } else {
                 data.name = xss(data.name)
                 data.content = xss(data.content)
                 await new CommentsModel(data).save()
             }
         } else {
-            error = '参数错误'
+            result = '参数错误'
         }
     } catch (err) {
-        error = JSON.stringify(err, null, 2)
+        result = JSON.stringify(err, null, 2)
     }
-    ctx.body = { error: error }
+    ctx.body = { result: result }
 }
 
 module.exports.getByPostId = async function(ctx, postId) {
