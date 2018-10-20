21e0bc6a1fbd98cb3e686675617afeed6e76102d
tujiaw <tujiaw@163.com>
2018-10-20 22:35:26 +0800

    poetry

---
+++ models/poetry.js
@@ -0,0 +1,46 @@
+const mongoose = require('./mongo').mongoose;
+
+const AuthorSongSchema = new mongoose.Schema({
+    name: { type: String },
+    short_description: { type: String },
+    description: { type: String }
+})
+
+const CiSongSchema = new mongoose.Schema({
+    author: { type: String },
+    rhythmic: { type: String },
+    paragraphs: { type: [String] }
+})
+
+const AuthorsSchema = new mongoose.Schema({
+    name: { type: String },
+    desc: { type: String }
+})
+
+const PoetSchema = new mongoose.Schema({
+    author: { type: String },
+    title: { type: String },
+    paragraphs: { type: [String] },
+    strains: { type: [String] }
+})
+
+const LunyuSchema = new mongoose.Schema({
+    chapter: { type: String },
+    paragraphs: { type: [String] }
+})
+
+const ShijingSchema = new mongoose.Schema({
+    title: { type: String },
+    chapter: { type: String },
+    section: { type: String },
+    content: { type: [String] }
+})
+
+module.exports.AuthorSongModel = mongoose.model('author_song', AuthorSongSchema);
+module.exports.CiSongModel = mongoose.model('ci_song', CiSongSchema);
+module.exports.AuthorsSongModel = mongoose.model('authors_song', AuthorsSchema);
+module.exports.AuthorsTangModel = mongoose.model('authors_tang', AuthorsSchema);
+module.exports.PoetSongModel = mongoose.model('peot_song', PoetSchema);
+module.exports.PoetTangModel = mongoose.model('peot_tang', PoetSchema);
+module.exports.LunyuModel = mongoose.model('lunyu', LunyuSchema);
+module.exports.ShijingModel = mongoose.model('shijing', ShijingSchema);
